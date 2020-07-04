6158ec854f823f123f8e306eb5dc0e4e7ef83b17
gttffv <66701766+gttffv@users.noreply.github.com>
2020-07-04 17:26:55 +0300

    تم الحل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -6,12 +6,26 @@
  * - name إن لم تستقبل اسم أو "Hello" تطبع
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
+function greet(name) {
+let massage = "hello"
+if(name){
+    massage=massage+=name;
+    console.log(massage);
 
-function greet(name) {}
-
+}else{console.log(massage)}
+}
+greet()
+greet("ahmed")
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+    if(n % 2 ==1){
+        return true;
+
+    }  else{
+        return false;
+    }
+}
